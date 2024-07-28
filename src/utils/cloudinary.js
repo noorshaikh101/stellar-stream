8828d8a84c9421b6618b6fe8433a51fbb1f93511
Noor Shaikh <noor101101@gmail.com>
2024-07-28 12:52:18 -0700

    multer setup

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,18 @@ cloudinary.config({
     api_key: process.env,CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        const response = await cloudinary.uploader.upload(localFilePath, {resource_type: "auto"})
+        console.log("File is uploaded on cloudinary." ,response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return  null;
+    }
+}
+
+export {uploadOnCloudinary}
